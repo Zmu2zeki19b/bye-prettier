6f63b5d337b1b1024396e65c0c54154bf76d3098
Ezequiel Blanco <ezequielgier19@gmail.com>
2025-01-24 20:34:18 +0000

    cartarandom

--- src/app.js
+++ src/app.js
@@ -5,7 +5,28 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
-  console.log("Chau prettier!!");
-};
+  let Numero = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  let Simbolo = ["♥", "♦", "♠", "♣"];
+
+  function generateRandomCarta() {
+    let randomNumero = Numero[Math.floor(Math.random() * Numero.length)];
+    let randomSimbolo = Simbolo[Math.floor(Math.random() * Simbolo.length)];
+
+    let NumeroElement = document.getElementById("Numero");
+    let SimbolotopElement = document.getElementById("Simbolo-top");
+    let SimbolodownElement = document.getElementById("Simbolo-down");
+
+    NumeroElement.textContent = randomNumero;
+    SimbolotopElement.textContent = randomSimbolo;
+    SimbolodownElement.textContent = randomSimbolo;
+
+    let Red = randomSimbolo === "♥" || randomSimbolo === "♦";
+    NumeroElement.style.color = Red ? "red" : "black";
+    SimbolotopElement.style.color = Red ? "red" : "black";
+    SimbolodownElement.style.color = Red ? "red" : "black";
+  }
+
+  generateRandomCarta();
+}
